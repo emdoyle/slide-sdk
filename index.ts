5c0f6dd54168669298ebfd62eb13a9049ad0d07f
Evan Doyle <evanmdoyle@gmail.com>
2022-03-16 11:03:02 -0700

    Remove Squads SDK from this package

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
-import * as squads from "./squads-sdk";
 import * as address from "./address";
 import * as utils from "./utils";
 import * as constants from "./constants";
 
-export { squads, address, utils, constants };
+export { address, utils, constants };
