9aba79db33daabdab8efc0608e48bf96a5a43b0f
Evan Doyle <evanmdoyle@gmail.com>
2022-03-17 22:40:00 -0700

    Export more in index

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as address from "./address";
 import * as utils from "./utils";
 import * as constants from "./constants";
+import { Slide } from "./types/slide";
 import { getSlide } from "./getSlide";
 
-export { address, utils, constants, getSlide };
+export { address, utils, constants, Slide, getSlide };
